9b375b0af5f5ba6815a31cb4461caf559d82276e
uccu <418667631@qq.com>
2016-02-03 10:01:03 +0800

    ProductInfoLogic改2

--- CES/Admin/Logic/ProductInfoLogic.class.php
+++ CES/Admin/Logic/ProductInfoLogic.class.php
@@ -2,7 +2,11 @@
 namespace Admin\Logic;
 use Think\Model;
 class ProductInfoLogic extends Model{
-	private function getList($page=1,$where=false){
+	public UserType;
+	public function _initialize(){
+		$this->UserType=D('CES/UserController')->getMyUserType();
+	}
+	private function getListByCondition($page=1,$where=false){
 		$product=M('ProductInfo');
 		$result=$product	->join('user_info on product_info.UserId = user_info.UserId')
 							->where($where)
@@ -20,22 +24,36 @@ class ProductInfoLogic extends Model{
 			return false;
 		}
 	}
-	private function update($proId,$data){
+	public function getList($page){
+		if($this->UserType<2)return false;
+		return $this->getListByCondition($page,'product_info.ProState < 4');
+	}
+	public function getListByDeleted($page){
+		if($this->UserType<2)return false;
+		return $this->getListByCondition($page,'product_info.ProState > 3');
+	}
+	public function update($proId,$data){
+		if($this->UserType<2)return false;
+		if($data['ProState'] && $this->UserType<3)return false;
 		return $this->updateByCondition(array('ProId'=>$proId),$data);
 	}
 	private function updateByCondition($where,$data){
 		return M('ProductInfo')->where($where)-save($data);
 	}
-	private function fakeDelete($proId){
+	public function fakeDelete($proId){
+		if($this->UserType<3)return false;
 		return $this->update($proId,array('ProState'=>4));
 	}
-	private function fakeDeleteByCondition($where){
+	public function fakeDeleteByCondition($where){
+		if($this->UserType<3)return false;
 		return $this->updateByCondition($where,array('ProState'=>4));
 	}
-	private function delete($proId){
+	public function delete($proId){
+		if($this->UserType<3)return false;
 		return $this->update($proId,array('ProState'=>5));
 	}
-	private function deleteByCondition($where){
+	public function deleteByCondition($where){
+		if($this->UserType<3)return false;
 		return $this->updateByCondition($where,array('ProState'=>5));
 	}
 }
