39b9594466c95198c843123977ba1562ec8f0287
uccu <418667631@qq.com>
2016-02-03 11:13:21 +0800

    productInfo

--- CES/Admin/Logic/ProductInfoLogic.class.php
+++ CES/Admin/Logic/ProductInfoLogic.class.php
@@ -2,11 +2,6 @@
 namespace Admin\Logic;
 use Think\Model;
 class ProductInfoLogic extends Model{
-	public UserType;
-	public function _initialize(){
-		$userInfo = I('session.admin',false);
-		$this->UserType = $userInfo===false ? $userInfo['UserType'] : -1;
-	}
 	private function getListByCondition($page=1,$where=false){
 		$result=$this	->join('user_info on product_info.UserId = user_info.UserId')
 						->where($where)
@@ -25,35 +20,27 @@ class ProductInfoLogic extends Model{
 		}
 	}
 	public function getList($page){
-		if($this->UserType<2)return false;
 		return $this->getListByCondition($page,'product_info.ProState < 4');
 	}
 	public function getListByDeleted($page){
-		if($this->UserType<2)return false;
 		return $this->getListByCondition($page,'product_info.ProState > 3');
 	}
 	public function update($proId,$data){
-		if($this->UserType<2)return false;
-		if($data['ProState'] && $this->UserType<3)return false;
 		return $this->updateByCondition(array('ProId'=>$proId),$data);
 	}
 	private function updateByCondition($where,$data){
 		return $this->where($where)-save($data);
 	}
 	public function fakeDelete($proId){
-		if($this->UserType<3)return false;
 		return $this->update($proId,array('ProState'=>4));
 	}
 	public function fakeDeleteByCondition($where){
-		if($this->UserType<3)return false;
 		return $this->updateByCondition($where,array('ProState'=>4));
 	}
 	public function delete($proId){
-		if($this->UserType<3)return false;
 		return $this->update($proId,array('ProState'=>5));
 	}
 	public function deleteByCondition($where){
-		if($this->UserType<3)return false;
 		return $this->updateByCondition($where,array('ProState'=>5));
 	}
 }
