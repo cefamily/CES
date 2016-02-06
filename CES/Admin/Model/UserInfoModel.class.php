0902be731ee36ce2b18600d564ed5028e2a2d3a4
democyann <dave_kura@outlook.com>
2016-02-06 20:34:24 +0800

    修改详细页面，修改自动验证规则

--- CES/Admin/Model/UserInfoModel.class.php
+++ CES/Admin/Model/UserInfoModel.class.php
@@ -5,10 +5,10 @@ use Think\Model;
 class UserInfoModel extends Model{
 	//protected $tableName = 'userinfo';
   	protected	$_validate=array(
-		array('UserName','require','用户名格式错误',1),
+		array('UserName','require','用户名格式错误',1,'',1),
 		array('UserName','4,16','用户名长度要在4-16字符',1,'length',1),
 		array('UserName','unique','该用户已存在',1,'unique',1),
-		array('UserPwd','require','请输入密码',1),
+		array('UserPwd','require','请输入密码',1,'',1),
 		array('UserEmail','email','Email格式不正确',1)
 	);
 	protected $_auto=array(
