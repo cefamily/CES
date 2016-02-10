a0e0fb2eda4fd8ed31ece69bfb289edb4f0fbd86
uccu <418667631@qq.com>
2016-02-10 17:52:21 +0800

    权限调整与注释

--- CES/Admin/Controller/ProductController.class.php
+++ CES/Admin/Controller/ProductController.class.php
@@ -1,6 +1,15 @@
 <?php
 namespace Admin\Controller;
 class ProductController extends ConstructController{
+	/*************************
+	** 需要模板
+	** 列表任务
+	** 模板提供变量
+	**     $pagedata	页面信息：最大页数count，当前页数now
+	**     $productlist	任务信息列表【数组】
+	** 支持search搜索，userid查询，state搜索（查询已删除需要管理员权限4）
+	** 格式为 URL+/page/页数/search/测试名字/state/2/userid/9 (顺序无关)
+	*************************/
 	public function showlist(){
 		$pagedata['now'] = $page = I('param.page',1,'int');
 		$product=D('ProductInfo','Logic');
@@ -15,6 +24,15 @@ class ProductController extends ConstructController{
 		$this->assign('item_index',0);
 		$this->display();
 	}
+	/*************************
+	** 需要模板
+	** 列表已删除任务（中途某些原因删除的任务）
+	** 需要权限管理员等级4
+	** 模板提供变量
+	**     $pagedata	页面信息：最大页数count，当前页数now
+	**     $productlist	任务信息列表【数组】
+	** 格式为 URL+/page/页数
+	*************************/
 	public function showlistOfDeleted(){
 		if($this->admintype<4)$this->error('无权限操作');
 		$pagedata['now'] = $page = I('param.page',1,'int');
@@ -26,18 +44,38 @@ class ProductController extends ConstructController{
 		$this->assign('item_index',1);
 		$this->display();
 	}
+	/*************************
+	** ajax and post
+	** 清理1个星期前的任务
+	** 需要管理员权限4
+	*************************/
 	public function clearProduct(){
+		if($this->admintype < 4)$this->error('无权限操作');
+		if(!IS_POST || !IS_AJAX)$this->error('清理失败');
 		D('ProductInfo','Logic')->deleteProOfAWeekAge();
 		$this->success('清理成功');
 	}
+	/*************************
+	** ajax and post
+	** 得到对应proid的任务信息
+	** 获取已删除任务需要管理员权限4
+	** 格式为 URL+/proid/任务ID
+	*************************/
 	public function getInfoByProId($proid){
-		if(!IS_POST)$this->error('获取失败');
+		if(!IS_POST || !IS_AJAX)$this->error('获取失败');
 		$info = D('ProductInfo','Logic')->getInfoByProId($proid);
 		if(!$info)$this->error('无法获取制定的任务信息');
+		elseif($info['state']>3 && $this->admintype < 4)$this->error('无权限操作');
 		else $this->success($info);
 	}
+	/*************************
+	** ajax and post
+	** 修改任务的信息
+	** 通过post发送任务修改内容，ProId必须
+	** 格式为{ProId:1,ProTitle:'测试',......}
+	*************************/
 	public function changeProductInfo(){
-		if(!IS_POST)$this->error('修改失败');
+		if(!IS_POST || !IS_AJAX)$this->error('修改失败');
 		$info = I('post.');
 		$proid = I('post.ProId',0,'int');
 		if(!$proid)$this->error('proId不存在');
