db19de532a18ab82bd8c0832421d034e07a897c0
democyann <dave_kura@outlook.com>
2016-02-13 11:12:16 +0800

    添加任务自动验证规则

--- CES/Admin/Model/ProductInfoModel.class.php
+++ CES/Admin/Model/ProductInfoModel.class.php
@@ -2,7 +2,11 @@
 namespace Admin\Model;
 use Think\Model;
 class ProductInfoModel extends Model {
-   
-   
-}
+   protected $_validate=array(
+   		array('UserId','require','用户ID必须'),
+   		array('ProTitle','require','标题必须',1),
+		array('ProImg','require','缩略图必须'),
+		array('ProRem','require','备注必须')
+		);
+		}
 ?> 
