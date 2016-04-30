3994184de9de283c0c4997e96982fbf2758fd40a
demoにゃん～ <dave_kura@outlook.com>
2016-04-30 23:51:12 +0800

    修改TeamEvent喵喵喵~

--- CES/Home/Event/TeamEvent.class.php
+++ CES/Home/Event/TeamEvent.class.php
@@ -9,8 +9,7 @@ class TeamEvent extends OutEvent{
     function _safe_control($pid){
 		$tid=$this->_safe_claim($pid);
         if($tid!=0){
-			$userInfo=session('userstat');
-			$user=M('UserTeam')->where(array('tid'=>$tid,'uid'=>$userInfo['uid'],'tadmin'=>'1'))->find();
+			$user=M('UserTeam')->where(array('tid'=>$tid,'uid'=>$this->user->uid,'tadmin'=>'1'))->find();
 			if($user){
 				return true;
 			}else{
@@ -26,9 +25,8 @@ class TeamEvent extends OutEvent{
     
     function _safe_claim($pid){
         $team=M('TeamUser');
-		$userInfo=session('userstat');
 		$prolist=M('Progress')->where(array('pid'=>$pid,'gtype'=>'team'))->getField('gtext');
-		$list=$team->where('uid='.$userInfo['uid'])->getField('uid');
+		$list=$team->where('uid='.$this->user->uid)->getField('uid');
         $flag=0;
 		foreach($list as $id){
 			if(in_array($id,$prolist)){
