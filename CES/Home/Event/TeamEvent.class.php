0eb9a014364d0c130a30c55da0ca21556b2c430f
demoにゃん～ <dave_kura@outlook.com>
2016-04-30 23:49:43 +0800

    TeamEvent完成

--- CES/Home/Event/TeamEvent.class.php
+++ CES/Home/Event/TeamEvent.class.php
@@ -7,15 +7,35 @@ class TeamEvent extends OutEvent{
     }
     
     function _safe_control($pid){
-        $this->user->uid;
+		$tid=$this->_safe_claim($pid);
+        if($tid!=0){
+			$userInfo=session('userstat');
+			$user=M('UserTeam')->where(array('tid'=>$tid,'uid'=>$userInfo['uid'],'tadmin'=>'1'))->find();
+			if($user){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
         
         
     }
     
     
     function _safe_claim($pid){
-        
-        
+        $team=M('TeamUser');
+		$userInfo=session('userstat');
+		$prolist=M('Progress')->where(array('pid'=>$pid,'gtype'=>'team'))->getField('gtext');
+		$list=$team->where('uid='.$userInfo['uid'])->getField('uid');
+        $flag=0;
+		foreach($list as $id){
+			if(in_array($id,$prolist)){
+				$flag=$id;
+			}
+		}
+		return $flag;
         
     }
 }
